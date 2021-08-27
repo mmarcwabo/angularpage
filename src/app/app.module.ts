7e0c470d83c5d211151d989a7697aee50a9d9815
marcwabo <marcwabo20@gmail.com>
2021-08-27 22:29:11 +0100

    Add Author and Category components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
+import { CategoryComponent } from './category/category.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
-    BlogPostComponent
+    BlogPostComponent,
+    CategoryComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
