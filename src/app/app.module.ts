0b3bd4d16d8173f2ca6b160a780e9f8b948f255a
marcwabo <marcwabo20@gmail.com>
2021-08-27 22:26:42 +0100

    Add BlogPost component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
