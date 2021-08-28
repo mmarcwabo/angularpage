c5a3a7b4a9266c7641f26e358acbe73cea2f52cb
marcwabo <marcwabo20@gmail.com>
2021-08-28 12:34:17 +0100

    Add title case pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { SummaryPipe } from './summary.pipe';
 import { AuthorService } from './author/author.service';
 import { CategoryService } from './category/category.service';
 import { NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { BlogPostComponent } from './blog-post/blog-post.component';
 import { CategoryComponent } from './category/category.component';
 import { AuthorComponent } from './author/author.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
@@ -18,11 +21,13 @@ import { FavoriteComponent } from './favorite/favorite.component';
     CategoryComponent,
     AuthorComponent,
     SummaryPipe,
-    FavoriteComponent
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CategoryService,
