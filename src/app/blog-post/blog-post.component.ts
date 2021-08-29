b5a8dd68ae00ef2f44dc67a11e86940726832834
marcwabo <marcwabo20@gmail.com>
2021-08-29 11:31:36 +0100

    Add directives for blog posts

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -9,7 +9,8 @@ export class BlogPostComponent implements OnInit {
 
   content = `
   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci sem, ornare non cursus in, venenatis eu lacus. Aenean tincidunt venenatis faucibus. Quisque accumsan urna nec massa volutpat interdum. Nulla sed sapien eros. Nunc at finibus est, id interdum diam. Fusce auctor accumsan dolor, ac vulputate lectus tincidunt nec. Vivamus luctus mauris a condimentum tempus. Suspendisse purus mauris, ultrices nec elementum non, luctus vel eros.
-`
+`;
+  blogs = ["Hello", "World"];
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/categories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+//decorator function
+@Component({
+    selector: 'category',
+    template: '<h3>Category</h3>'
+})
+export class CategoriesComponent{
+
+}
