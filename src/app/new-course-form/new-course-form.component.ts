e6320f8a3cdfd37e76ea9edee52f4189f3fa781d
marcwabo <marcwabo20@gmail.com>
2021-08-31 12:46:42 +0100

    Add new-course-form component

---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent {
+
+  categories = [
+    {id:1, name:'Developement'},
+    {id:2, name:'Art'},
+    {id:3, name:'Languages'},
+  ];
+
+
+}
