957d4d509b2330e9925bc2473d2870632e8583ae
marcwabo <marcwabo20@gmail.com>
2021-09-01 00:11:45 +0100

    Update new-course-form component

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class NewCourseFormComponent {
 
   categories = [
-    {id:1, name:'Developement'},
-    {id:2, name:'Art'},
-    {id:3, name:'Languages'},
+    { id: 1, name: 'Developement' },
+    { id: 2, name: 'Art' },
+    { id: 3, name: 'Languages' },
   ];
+
+  constructor() {
+
+  }
+
+  submit(course: any) {
+   console.log(course);
+  }
 
 
 }
